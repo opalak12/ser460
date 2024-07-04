69d46202a83620c8be96acf74dc995c50d03e77a
pojha12 <palak@MacBook-Pro.attlocal.net>
2024-07-04 00:37:55 -0500

    removing unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import Login from './components/Login';
 import PublishMeals from './components/Publish';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Subscribe from './components/Subscribe';
-import Search from './components/search'
 
 function App() {
   return (
     <Router>
     <Navbar />
     <Routes>
-      <Route path="/login" element={<Login />} />
       <Route path="/publish" element={<PublishMeals />} />
       <Route path="/subscribe" element={<Subscribe />} />
-      <Route path="/search" element={<Search />} />
     </Routes>
   </Router>
   );
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import "./styles.css";
 import { RxAvatar } from "react-icons/rx";
 import { useNavigate } from 'react-router-dom';
@@ -6,13 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 function Navbar(){
     const [isOpen, setIsOpen] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const navigate = useNavigate();
-
-    useEffect(() => {
-        const loggedIn = localStorage.getItem('isLoggedIn') === 'true';
-        setIsLoggedIn(loggedIn);
-      }, []);
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
@@ -20,14 +14,8 @@ function Navbar(){
       const handleNavigation = (path) => {
         navigate(path);
         setIsOpen(false)
-        setIsLoggedIn(true)
       };
 
-      const handleLogout = () => {
-        localStorage.removeItem('isLoggedIn');
-        setIsLoggedIn(false);
-        navigate('/');
-      };
 
     return (
         <div className='navbar'>
@@ -38,19 +26,6 @@ function Navbar(){
                 </button>
                 {isOpen && (
                 <ul className='dropdown-menu'>
-
-                    {!isLoggedIn && (
-                    <li>
-                        <button onClick={() => handleNavigation('/login')}>Login</button>
-                    </li>
-                    )}
-
-                    {isLoggedIn && (
-                        <li>
-                            <button onClick={handleLogout}>Logout</button>
-                        </li>
-                    )}
-
                     <li>
                     <button onClick={() => handleNavigation('/publish')}>Add meal</button>
                     </li>
