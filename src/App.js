5acf738eece0d7fb7e34d61b6bd39a0119dc1406
vknezevi <vknezevi@asu.edu>
2024-07-03 23:11:10 -0500

    Landing Page, Publishing Daily and Weekly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import DailyPlanPage from './components/DailyPlanPage';
+// import WeeklyPlanPage from './WeeklyPlanPage';
+import LandingPage from './components/LandingPage';
+import WeeklyPlanPage from './components/WeeklyPlanPage';
 
 function App() {
   return (
-   <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React, Vlad
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/daily-plan" element={<DailyPlanPage />} />
+      <Route path="/weekly-plan" element={<WeeklyPlanPage />} />
+    </Routes>
+  </Router>
   );
 }
 
